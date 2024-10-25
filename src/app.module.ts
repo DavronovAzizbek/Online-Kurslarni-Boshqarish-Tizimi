90d090831a9973a83ad0280c2013b5d4f6c5d72f
githubusername <azizbekdavronov1110@gmail.com>
2024-10-26 00:21:53 +0500

    Dockerfile va docker-compose.yml yaratildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from './admin/admin.module';
 import { CoursesModule } from './courses/courses.module';
 import { ModulesModule } from './modules/modules.module';
@@ -10,8 +9,28 @@ import { AssignmentsModule } from './assignments/assignments.module';
 import { ResultsModule } from './results/results.module';
 
 @Module({
-  imports: [AuthModule, AdminModule, CoursesModule, ModulesModule, LessonsModule, AssignmentsModule, ResultsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: +process.env.DATABASE_PORT || 5432,
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'azizbek002',
+      database: process.env.DATABASE_NAME || '7410',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    AdminModule,
+    CoursesModule,
+    ModulesModule,
+    LessonsModule,
+    AssignmentsModule,
+    ResultsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
