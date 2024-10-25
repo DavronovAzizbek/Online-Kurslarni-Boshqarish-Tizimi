aa14849a13a1eff008f92f6f0a473a84a3b58209
githubusername <azizbekdavronov1110@gmail.com>
2024-10-25 23:36:11 +0500

    online-kurs-tizimi loyihasi yaratildi va kerakli modullar ham yaratildi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { CoursesModule } from './courses/courses.module';
+import { ModulesModule } from './modules/modules.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { ResultsModule } from './results/results.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AdminModule, CoursesModule, ModulesModule, LessonsModule, AssignmentsModule, ResultsModule],
   controllers: [AppController],
   providers: [AppService],
 })
